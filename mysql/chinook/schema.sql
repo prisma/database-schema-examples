ff5bea5fec127d0548ff86f16a020c3fe03fef89
Jan Piotrowski <piotrowski+github@gmail.com>
2020-02-06 19:27:17 +0100

    Add `AUTO_INCREMENT` (via Chinook_MySql_AutoIncrementPKs.sql) Add `ENGINE=InnoDB` Remove `DATABASE` and `USE`

--- mysql/chinook/schema.sql
+++ mysql/chinook/schema.sql
@@ -1,33 +1,34 @@
 /*******************************************************************************
    Chinook Database - Version 1.4
-   Script: Chinook_MySql.sql
+   Script: Chinook_MySql_AutoIncrementPKs.sql (schema) + Chinook_MySql.sql (data)
    Description: Creates and populates the Chinook database.
    DB Server: MySql
    Author: Luis Rocha
    License: http://www.codeplex.com/ChinookDatabase/license
 ********************************************************************************/
 
+
 /*******************************************************************************
    Create Tables
 ********************************************************************************/
 CREATE TABLE `Album`
 (
-    `AlbumId` INT NOT NULL,
+    `AlbumId` INT NOT NULL AUTO_INCREMENT,
     `Title` NVARCHAR(160) NOT NULL,
     `ArtistId` INT NOT NULL,
     CONSTRAINT `PK_Album` PRIMARY KEY  (`AlbumId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Artist`
 (
-    `ArtistId` INT NOT NULL,
+    `ArtistId` INT NOT NULL AUTO_INCREMENT,
     `Name` NVARCHAR(120),
     CONSTRAINT `PK_Artist` PRIMARY KEY  (`ArtistId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Customer`
 (
-    `CustomerId` INT NOT NULL,
+    `CustomerId` INT NOT NULL AUTO_INCREMENT,
     `FirstName` NVARCHAR(40) NOT NULL,
     `LastName` NVARCHAR(20) NOT NULL,
     `Company` NVARCHAR(80),
@@ -41,11 +42,11 @@ CREATE TABLE `Customer`
     `Email` NVARCHAR(60) NOT NULL,
     `SupportRepId` INT,
     CONSTRAINT `PK_Customer` PRIMARY KEY  (`CustomerId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Employee`
 (
-    `EmployeeId` INT NOT NULL,
+    `EmployeeId` INT NOT NULL AUTO_INCREMENT,
     `LastName` NVARCHAR(20) NOT NULL,
     `FirstName` NVARCHAR(20) NOT NULL,
     `Title` NVARCHAR(30),
@@ -61,18 +62,18 @@ CREATE TABLE `Employee`
     `Fax` NVARCHAR(24),
     `Email` NVARCHAR(60),
     CONSTRAINT `PK_Employee` PRIMARY KEY  (`EmployeeId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Genre`
 (
-    `GenreId` INT NOT NULL,
+    `GenreId` INT NOT NULL AUTO_INCREMENT,
     `Name` NVARCHAR(120),
     CONSTRAINT `PK_Genre` PRIMARY KEY  (`GenreId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Invoice`
 (
-    `InvoiceId` INT NOT NULL,
+    `InvoiceId` INT NOT NULL AUTO_INCREMENT,
     `CustomerId` INT NOT NULL,
     `InvoiceDate` DATETIME NOT NULL,
     `BillingAddress` NVARCHAR(70),
@@ -82,42 +83,42 @@ CREATE TABLE `Invoice`
     `BillingPostalCode` NVARCHAR(10),
     `Total` NUMERIC(10,2) NOT NULL,
     CONSTRAINT `PK_Invoice` PRIMARY KEY  (`InvoiceId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `InvoiceLine`
 (
-    `InvoiceLineId` INT NOT NULL,
+    `InvoiceLineId` INT NOT NULL AUTO_INCREMENT,
     `InvoiceId` INT NOT NULL,
     `TrackId` INT NOT NULL,
     `UnitPrice` NUMERIC(10,2) NOT NULL,
     `Quantity` INT NOT NULL,
     CONSTRAINT `PK_InvoiceLine` PRIMARY KEY  (`InvoiceLineId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `MediaType`
 (
-    `MediaTypeId` INT NOT NULL,
+    `MediaTypeId` INT NOT NULL AUTO_INCREMENT,
     `Name` NVARCHAR(120),
     CONSTRAINT `PK_MediaType` PRIMARY KEY  (`MediaTypeId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Playlist`
 (
-    `PlaylistId` INT NOT NULL,
+    `PlaylistId` INT NOT NULL AUTO_INCREMENT,
     `Name` NVARCHAR(120),
     CONSTRAINT `PK_Playlist` PRIMARY KEY  (`PlaylistId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `PlaylistTrack`
 (
     `PlaylistId` INT NOT NULL,
     `TrackId` INT NOT NULL,
     CONSTRAINT `PK_PlaylistTrack` PRIMARY KEY  (`PlaylistId`, `TrackId`)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE `Track`
 (
-    `TrackId` INT NOT NULL,
+    `TrackId` INT NOT NULL AUTO_INCREMENT,
     `Name` NVARCHAR(200) NOT NULL,
     `AlbumId` INT,
     `MediaTypeId` INT NOT NULL,
@@ -127,7 +128,7 @@ CREATE TABLE `Track`
     `Bytes` INT,
     `UnitPrice` NUMERIC(10,2) NOT NULL,
     CONSTRAINT `PK_Track` PRIMARY KEY  (`TrackId`)
-);
+) ENGINE=InnoDB;
 
 
 
