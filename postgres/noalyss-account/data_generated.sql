0751229b973d7f601de423f0a8240486678b0e69
Jan Piotrowski <piotrowski+github@gmail.com>
2020-03-10 23:52:44 +0100

    postgres data

---
+++ postgres/noalyss-account/data_generated.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:685ed96d9eb3b673006d2012c47c3049374fc269548341f0921733724de549a6
+size 144301
---
+++ postgres/noalyss-folder/data_generated.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:123bd63b22e224e3047323b969c19cb88a7b6d260fcd77ad0fd1838675186963
+size 847285
