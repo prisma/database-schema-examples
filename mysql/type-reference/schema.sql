960d28cfdeb64e7544c98696f56a0a3aa2a1bff5
Jan Piotrowski <piotrowski+github@gmail.com>
2021-12-14 22:07:22 +0000

    moar details on mysql

--- mysql/type-reference/schema.sql
+++ mysql/type-reference/schema.sql
@@ -7,16 +7,17 @@ CREATE TABLE `types` (
   `numeric_integer_int` INT NOT NULL,
   `numeric_integer_bigint` BIGINT NOT NULL,
   `numeric_fixed_decimal` DECIMAL NOT NULL,
+  `numeric_fixed_numeric` NUMERIC NOT NULL, -- alias to decimal
   `numeric_floating_float` FLOAT NOT NULL,
   `numeric_floating_double` DOUBLE NOT NULL,
   `numeric_floating_real` REAL NOT NULL,
   `numeric_bit` BIT NOT NULL,
   `numeric_boolean` BOOL NOT NULL,
-  `date_date` date NOT NULL,
-  `date_datetime` datetime NOT NULL,
-  `date_timestamp` timestamp NOT NULL,
-  `date_time` time NOT NULL,
-  `date_year` year(4) NOT NULL,
+  `date_date` DATE NOT NULL,
+  `date_datetime` DATETIME NOT NULL,
+  `date_timestamp` TIMESTAMP NOT NULL,
+  `date_time` TIME NOT NULL,
+  `date_year` YEAR NOT NULL,
   `string_char` char(255) NOT NULL,
   `string_varchar` varchar(255) NOT NULL,
   `string_text_tinytext` tinytext NOT NULL,
@@ -43,16 +44,10 @@ CREATE TABLE `types` (
   PRIMARY KEY (`id`)
 );
 
--- https://dev.mysql.com/doc/refman/8.0/en/timestamp-initialization.html
-CREATE TABLE `types_timestamp_initialization` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `date_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-);
+-- TODO Defaults for all columns
 
 -- https://dev.mysql.com/doc/refman/8.0/en/fixed-point-types.html
-CREATE TABLE `types_fixed_point` (
+CREATE TABLE `types_numeric_fixed` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `decimal` DECIMAL NOT NULL,
   `decimal_10` DECIMAL(10) NOT NULL,
@@ -62,11 +57,76 @@ CREATE TABLE `types_fixed_point` (
 );
 
 -- https://dev.mysql.com/doc/refman/8.0/en/floating-point-types.html
-TODO
-CREATE TABLE `types_floating_point` (
+CREATE TABLE `types_numeric_floating` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `numeric_floating_float` FLOAT NOT NULL,
-  `numeric_floating_double` DOUBLE NOT NULL,
-  `numeric_floating_real` REAL NOT NULL,
+  `numeric_floating_float_10` FLOAT(10) NOT NULL,
+  `numeric_floating_float_50` FLOAT(50) NOT NULL,
+  `numeric_floating_double_10` DOUBLE(10) NOT NULL,
+  `numeric_floating_double_50` DOUBLE(50) NOT NULL,
+  `numeric_floating_real_10` REAL(10) NOT NULL,
+  `numeric_floating_real_50` REAL(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- https://dev.mysql.com/doc/refman/8.0/en/bit-type.html
+CREATE TABLE `types_numeric_bit` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `numeric_bit_10` BIT(10) NOT NULL,
+  `numeric_bit_50` BIT(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- https://dev.mysql.com/doc/refman/8.0/en/numeric-type-attributes.html
+CREATE TABLE `types_numeric_attributes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  -- display width
+  `numeric_integer_tinyint_width_1` TINYINT(1) UNSIGNED NOT NULL,
+  `numeric_integer_smallint_width_2` SMALLINT(2) UNSIGNED NOT NULL,
+  `numeric_integer_mediumint_width_3` MEDIUMINT(3) UNSIGNED NOT NULL,
+  `numeric_integer_int_width_4` INT(4) UNSIGNED NOT NULL,
+  `numeric_integer_bigint_width_5` BIGINT(5) UNSIGNED NOT NULL,
+  -- unsigned
+  `numeric_integer_tinyint_unsigned` TINYINT UNSIGNED NOT NULL,
+  `numeric_integer_smallint_unsigned` SMALLINT UNSIGNED NOT NULL,
+  `numeric_integer_mediumint_unsigned` MEDIUMINT UNSIGNED NOT NULL,
+  `numeric_integer_int_unsigned` INT UNSIGNED NOT NULL,
+  `numeric_integer_bigint_unsigned` BIGINT UNSIGNED NOT NULL,
+  -- auto_increment
+  `numeric_integer_tinyint_auto_increment` TINYINT AUTO_INCREMENT NOT NULL,
+  `numeric_integer_smallint_auto_increment` SMALLINT AUTO_INCREMENT NOT NULL,
+  `numeric_integer_mediumint_auto_increment` MEDIUMINT AUTO_INCREMENT NOT NULL,
+  `numeric_integer_int_auto_increment` INT AUTO_INCREMENT NOT NULL,
+  `numeric_integer_bigint_auto_increment` BIGINT AUTO_INCREMENT NOT NULL,
+  `numeric_fixed_decimal_auto_increment` DECIMAL AUTO_INCREMENT NOT NULL,
+  `numeric_fixed_numeric_auto_increment` NUMERIC AUTO_INCREMENT NOT NULL, -- alias to decimal
+  `numeric_floating_float_auto_increment` FLOAT AUTO_INCREMENT NOT NULL, -- deprecated in MySQL 8
+  `numeric_floating_double_auto_increment` DOUBLE AUTO_INCREMENT NOT NULL, -- deprecated in MySQL 8
+  `numeric_floating_real_auto_increment` REAL AUTO_INCREMENT NOT NULL, -- deprecated in MySQL 8
+  PRIMARY KEY (`id`)
+);
+
+-- https://dev.mysql.com/doc/refman/8.0/en/date-and-time-type-syntax.html
+CREATE TABLE `types_date_fractional_seconds_precision` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_datetime_0` DATETIME(0) NOT NULL,
+  `date_datetime_3` DATETIME(3) NOT NULL,
+  `date_datetime_6` DATETIME(6) NOT NULL,
+  `date_timestamp_0` TIMESTAMP(0) NOT NULL,
+  `date_timestamp_3` TIMESTAMP(3) NOT NULL,
+  `date_timestamp_6` TIMESTAMP(6) NOT NULL,
+  `date_time_0` TIME(0) NOT NULL,
+  `date_time_3` TIME(3) NOT NULL,
+  `date_time_6` TIME(6) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- TODO date defaults with fsp
+
+
+-- https://dev.mysql.com/doc/refman/8.0/en/timestamp-initialization.html
+CREATE TABLE `types_date_timestamp_initialization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
